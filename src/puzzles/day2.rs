345cb50c0f4a4f70f69c48d24d7faf44210179c0
Nate Brady <nathan.brady@yahoo.com>
2020-12-01 23:38:21 -0800

    day 2

--- src/puzzles/day2.rs
+++ src/puzzles/day2.rs
@@ -1,8 +1,40 @@
 use crate::utils::*;
 
+fn get_min_max(s: &str) -> (usize, usize) {
+    let parts: Vec<usize> = s.split("-").map(|i| i.parse().unwrap_or(0)).collect();
+    (parts[0], parts[1])
+}
+
+fn part1(input: &Vec<&str>) {
+    let mut valid = 0;
+    for i in input.iter() {
+        let parts: Vec<&str> = i.split(" ").collect();
+        let count = parts[2].matches(&parts[1][0..1]).count();
+        let (min, max) = get_min_max(parts[0]);
+        if (min..=max).contains(&count) {
+            valid += 1;
+        }
+    }
+    println!(" Part 1: {}", valid);
+}
+
+fn part2(input: &Vec<&str>) {
+    let mut valid = 0;
+    for i in input.iter() {
+        let parts: Vec<&str> = i.split(" ").collect();
+        let (min, max) = get_min_max(parts[0]);
+        if (parts[2][(min - 1)..min] == parts[1][0..1]) ^ (parts[2][(max - 1)..max] == parts[1][0..1]) {
+            valid += 1;
+        }
+    }
+    println!(" Part 2: {}", valid);
+}
+
 // https://adventofcode.com/2020/day/2
 pub fn solve() {
     println!("** Day 2 **");
     let input = read_file("day2-input.txt");
-    let input: Vec<i64> = input.to_vec_i64();
+    let input: Vec<&str> = input.split("\n").collect();
+    part1(&input);
+    part2(&input);
 }
