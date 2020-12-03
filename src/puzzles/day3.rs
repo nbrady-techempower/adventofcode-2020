1fb85abca7481677c826c2e627030b7fab1e5470
Nate Brady <nathan.brady@yahoo.com>
2020-12-03 12:00:12 -0800

    day 3

--- src/puzzles/day3.rs
+++ src/puzzles/day3.rs
@@ -1,11 +1,9 @@
 use crate::utils::*;
 
 fn get_trees(input: &Vec<&str>, slope: (usize, usize)) -> i64 {
-    let mut right = 0;
-    let mut num_trees = 0;
+    let (mut right, mut num_trees) = (0, 0);
     for i in (slope.1..input.len()).step_by(slope.1) {
-        right += slope.0;
-        right = right % input[i].len();
+        right = (right + slope.0) % input[i].len();
         num_trees += (input[i].chars().nth(right) == Some('#')) as i64;
     }
     num_trees
