754b399331a6b7e0f366ef47e84611a8a52f91f5
Nate Brady <nathan.brady@yahoo.com>
2020-12-03 12:01:42 -0800

    day 3

--- src/puzzles/day3.rs
+++ src/puzzles/day3.rs
@@ -9,19 +9,11 @@ fn get_trees(input: &Vec<&str>, slope: (usize, usize)) -> i64 {
     num_trees
 }
 
-fn part1(input: &Vec<&str>) {
-    println!(" Part 1: {:?}", get_trees(input, (3, 1)));
-}
-
-fn part2(input: &Vec<&str>) {
-    println!(" Part 2: {:?}", vec![(1,1), (3,1), (5,1), (7,1), (1,2)].iter().fold(1, |mut total, val| total * get_trees(input, *val)));
-}
-
 // https://adventofcode.com/2020/day/3
 pub fn solve() {
     println!("** Day 3 **");
     let input = read_file("day3-input.txt");
     let input: Vec<&str> = input.split("\n").collect();
-    part1(&input);
-    part2(&input);
+    println!(" Part 1: {:?}", get_trees(&input, (3, 1)));
+    println!(" Part 2: {:?}", vec![(1,1), (3,1), (5,1), (7,1), (1,2)].iter().fold(1, |mut total, val| total * get_trees(&input, *val)));
 }
