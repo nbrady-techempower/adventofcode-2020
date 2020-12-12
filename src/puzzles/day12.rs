5b88af5ac76447d06508845ef9f6c25cebbcdf90
Nate Brady <nathan.brady@yahoo.com>
2020-12-12 00:50:47 -0800

    day 12

--- src/puzzles/day12.rs
+++ src/puzzles/day12.rs
@@ -1,47 +1,37 @@
 use crate::*;
 
-fn lets_go(part: u8, input: Vec<String>) {
-
+fn part1(input: Vec<String>) {
     let mut facing = ["E", "S", "W", "N"];
     let mut x = 0;
     let mut y = 0;
 
     for i in input {
-        let d = i[0..1].as_ref();
-        let mut num = i32!(i[1..].to_string());
+        let mut d = i[0..1].as_ref();
+        let num = i32!(i[1..].to_string());
 
         match d {
             "R" => facing.rotate_left((num / 90) as usize),
-            "L" => {
-                while num > 0 {
-                    facing.rotate_left(3);
-                    num -= 90;
+            "L" => facing.rotate_right((num / 90) as usize),
+            _ => {
+                if d == "F" {
+                    d = facing[0];
                 }
-            },
-            "S" => y -= num,
-            "N" => y += num,
-            "E" => x += num,
-            "W" => x -= num,
-            "F" => {
-                match facing[0] {
+                match d {
                     "S" => y -= num,
                     "N" => y += num,
                     "E" => x += num,
                     "W" => x -= num,
                     _ => ()
                 }
-            },
-            _ => ()
+            }
         }
 
     }
-    println!(" Part {}: {}", part, x.abs() + y.abs());
+    part1!(x.abs() + y.abs());
 }
 
-fn lets_go2(part: u8, input: Vec<String>) {
-
+fn part2(input: Vec<String>) {
     let mut waypoint = vec![1, 10, 0, 0];
-
     let mut x = 0;
     let mut y = 0;
 
@@ -66,13 +56,13 @@ fn lets_go2(part: u8, input: Vec<String>) {
         }
 
     }
-    println!(" Part {}: {}", part, x.abs() + y.abs());
+    part2!(x.abs() + y.abs());
 }
 
 
 // https://adventofcode.com/2020/day/12
 pub fn solve(input: String) {
     let input: Vec<String> = input.split("\n").map(|e| e.to_string()).collect();
-    lets_go(1, input.clone());
-    lets_go2(2, input.clone());
+    part1(input.clone());
+    part2( input.clone());
 }
