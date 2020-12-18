1fd487a539a0f7e84691e7f68f01ac94787f6ada
Nate Brady <nathan.brady@yahoo.com>
2020-12-18 08:32:29 -0800

    day 18

--- src/puzzles/day18.rs
+++ src/puzzles/day18.rs
@@ -1,116 +1,59 @@
 use crate::*;
-use std::collections::HashMap;
 
-fn get_sum(s: &str) -> i64 {
-    let mut to_ret = 0;
-    let mut is_val1 = true;
-    let mut val1 = "".to_string();
-    let mut op = '+';
-    for c in s.chars() {
-        match c {
-            ' ' => {
-              if val1.len() == 0 {
-                continue;
-              }
-              if is_val1 {
-                  is_val1 = false;
-                  to_ret = i64!(val1);
-              } else {
-                  if op == '+' {
-                      to_ret += i64!(val1);
-                  } else {
-                      to_ret *= i64!(val1);
-                  }
-              }
-                val1 = "".to_string();
-            },
-            '+'|'*' => op = c,
-            _ => {
-                val1 = string!(val1, c);
-            }
-        }
-    }
-    if op == '+' {
-        to_ret += i64!(val1);
-    } else {
-        to_ret *= i64!(val1);
-    }
-    to_ret
-}
-
-fn get_sum2(s: &str) -> i64 {
+fn get_sum2(s: &str, part: u8) -> i64 {
     let mut parts = s.split(' ').into_iter().map(|c| c.to_string()).collect::<Vec<String>>();
-    'top: loop {
-        let tmp = parts.clone();
-        if tmp.into_iter().filter(|c| c == &"+").collect::<Vec<String>>().len() == 0 {
-            break;
+    let mut more_to_add = false;
+    'top: while parts.len() != 1 {
+        if part == 2 && parts.clone().into_iter().filter(|c| c == &"+").collect::<Vec<String>>().len() > 0 {
+            more_to_add = true;
+        } else if part == 2 {
+            more_to_add = false;
         }
         for i in (0..parts.len()).step_by(2) {
+            if !["+", "*"].contains(&parts[i+1].as_str()) || (more_to_add && &parts[i+1] == "*") {
+                continue;
+            }
+            let mut new_part = "".to_string();
             if parts[i+1] == "+" {
-                let new_part = (i64!(parts[i]) + i64!(parts[i+2])).to_string();
-                parts.remove(i);
-                parts.remove(i);
-                parts.remove(i);
-                parts.insert(i, new_part);
-                continue 'top;
+                new_part = (i64!(parts[i]) + i64!(parts[i+2])).to_string();
+            } else if parts[i+1] == "*" {
+                new_part = (i64!(parts[i]) * i64!(parts[i+2])).to_string();
             }
+            parts.remove(i);
+            parts.remove(i);
+            parts.remove(i);
+            parts.insert(i, new_part);
+            continue 'top;
         }
     }
-    parts.into_iter().filter(|c| c != &"*").fold(1, |acc, c| acc * i64!(c))
+    i64!(parts[0])
 }
 
-fn part1(input: Vec<String>) {
+fn lets_go(input: Vec<String>, part: u8) {
     let mut to_ret = 0;
 
     for i in input {
         let mut s = i.clone();
         while s.contains('(') {
-            let mut idx1 = 0;
-            let mut idx2 = 0;
+            let (mut idx1, mut idx2) = (0, 0);
             for (i, c) in s.chars().enumerate() {
                 if c == '(' {
                     idx1 = i;
-                }
-                if c == ')' {
+                } else if c == ')' {
                     idx2 = i;
                     break;
                 }
             }
-            s = string!(&s[0..idx1], get_sum(&s[idx1+1..idx2]), &s[idx2+1..]);
+            s = string!(&s[0..idx1], get_sum2(&s[idx1+1..idx2], part), &s[idx2+1..]);
         }
-        to_ret += get_sum(&s[0..])
+        to_ret += get_sum2(&s[0..], part)
     }
-    part1!(to_ret);
+    println!(" Part {}: {}", part, to_ret);
 }
-
-fn part2(input: Vec<String>) {
-    let mut to_ret = 0;
-
-    for i in input {
-        let mut s = i.clone();
-        while s.contains('(') {
-            let mut idx1 = 0;
-            let mut idx2 = 0;
-            for (i, c) in s.chars().enumerate() {
-                if c == '(' {
-                    idx1 = i;
-                }
-                if c == ')' {
-                    idx2 = i;
-                    break;
-                }
-            }
-            s = string!(&s[0..idx1], get_sum2(&s[idx1+1..idx2]), &s[idx2+1..]);
-        }
-        to_ret += get_sum2(&s[0..])
-    }
-    part2!(to_ret);
-}
-
 
 // https://adventofcode.com/2020/day/18
 pub fn solve(input: String) {
     let input: Vec<String> = input.split("\n").map(|e| e.to_string()).collect();
-    part1(input.clone());
-    part2(input.clone());
+    lets_go(input.clone(), 1);
+    lets_go(input.clone(), 2);
 }
